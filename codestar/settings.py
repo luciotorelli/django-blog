78e0191019d4d55a48aff01d0c269ca1494e2804
Lúcio Torelli <luciotorelli@hotmail.com>
2023-07-10 12:56:27 +0000

    Fix Heroku allowed hosts error

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-luciotorelli-djangoblog-tzzw8bd8jvl.ws-eu101.gitpod.io', 'django-blog-lucio.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-luciotorelli-djangoblog-tzzw8bd8jvl.ws-eu101.gitpod.io', 'django-blog-lucio-a55429cda10f.herokuapp.com', 'localhost']
 
 
 # Application definition
