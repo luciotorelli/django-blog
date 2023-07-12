3046a02c1d2f94675faec4153d8e2cd187f1a0f5
Lúcio Torelli <luciotorelli@hotmail.com>
2023-07-12 10:50:26 +0000

    Heroku deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,9 +29,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-luciotorelli-djangoblog-tzzw8bd8jvl.ws-eu101.gitpod.io', 'localhost']
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Application definition
 
